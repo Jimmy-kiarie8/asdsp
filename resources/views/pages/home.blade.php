fd1c2622dceb078d307e0183bdabe311f2bc16e9
jimmy <jimmy@kali.kali>
2023-12-19 15:01:38 +0300

    commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -382,13 +382,6 @@
                     </div>
 
 
-
-
-
-
-
-
-
                     <span class="font-semibold text-gray-400 uppercase">OUR PARTNERS</span>
                     <div class="slideshow-container">
                         <!-- Slides -->
@@ -501,83 +494,83 @@
                     </div> --}}
 
 
-                   
-        <div class="grid md:grid-cols-2 gap-8">
 
-            <div
-                class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-8 md:p-12">
-                <a href="#"
-                    class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md light:bg-gray-700 light:text-green-400 mb-2">
-                    <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor" viewBox="0 0 18 18">
-                        <path
-                            d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
-                    </svg>
-                    ASDSP
-                </a>
-                <h2 class="text-gray-900 light:text-white text-3xl font-extrabold mb-2">Success Stories</h2>
-                <p class="text-lg font-normal text-gray-500 light:text-gray-400 mb-4">“Better opportunities
-                    and tools to enable poor
-                    people to improve their living conditions” The programme aims at transforming crop,
-                    livestock and fishery
-                    production into commercially oriented enterprises that ensure sustainable food and
-                    nutrition security by
-                    developing value chains that are environmentally resilient and socially inclusive.
+                    <div class="grid md:grid-cols-2 gap-8">
 
-                    The stories briefly describe the groups
-                    and in some cases the individuals, the challenges they faced, the key programme
-                    interventions and the
-                    resulting changes that the beneficiaries groups experienced that improved their
-                    livelihoods.</p>
-                <a href="/media/docs/NEWSLETTER_12.pdf" target="_blank"
-                    class="text-green-600 light:text-green-500 hover:underline font-medium text-lg inline-flex items-center">Read
-                    more
-                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
-            </div>
+                        <div
+                            class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-8 md:p-12">
+                            <a href="#"
+                                class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md light:bg-gray-700 light:text-green-400 mb-2">
+                                <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 18 18">
+                                    <path
+                                        d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
+                                </svg>
+                                ASDSP
+                            </a>
+                            <h2 class="text-gray-900 light:text-white text-3xl font-extrabold mb-2">Success Stories</h2>
+                            <p class="text-lg font-normal text-gray-500 light:text-gray-400 mb-4">“Better opportunities
+                                and tools to enable poor
+                                people to improve their living conditions” The programme aims at transforming crop,
+                                livestock and fishery
+                                production into commercially oriented enterprises that ensure sustainable food and
+                                nutrition security by
+                                developing value chains that are environmentally resilient and socially inclusive.
 
-            <div
-                class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-8 md:p-12">
+                                The stories briefly describe the groups
+                                and in some cases the individuals, the challenges they faced, the key programme
+                                interventions and the
+                                resulting changes that the beneficiaries groups experienced that improved their
+                                livelihoods.</p>
+                            <a href="/media/docs/NEWSLETTER_12.pdf" target="_blank"
+                                class="text-green-600 light:text-green-500 hover:underline font-medium text-lg inline-flex items-center">Read
+                                more
+                                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
 
-                {{-- <img class="rounded-t-lg"
+                        <div
+                            class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-8 md:p-12">
+
+                            {{-- <img class="rounded-t-lg"
                     src="https://scontent.fmba5-1.fna.fbcdn.net/v/t39.30808-6/411417745_664223892559927_8480095982472557992_n.jpg?stp=dst-jpg_p180x540&_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeELaMNOL3NA1AawY9NN08EmK43E1l_MfbErjcTWX8x9sYEhMynl2q7c2O9FfCcpuzYBhuDatJDGSTayxwHS_7Dz&_nc_ohc=rGaZs3Xko2wAX_4G0gZ&_nc_ht=scontent.fmba5-1.fna&oh=00_AfDk0AXjZBR4eG86fhPnuuWy4ytOvgWjd2GYcyYtlv96rg&oe=6585B48E"
                     alt="" id="innovation-img" /> --}}
-                <a href="#"
-                    class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md light:bg-gray-700 light:text-green-400 mb-2">
-                    <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor" viewBox="0 0 18 18">
-                        <path
-                            d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
-                    </svg>
-                    ASDSP
-                </a>
-                <h2 class="text-gray-900 light:text-white text-3xl font-extrabold mb-2">Kenya News Agency -
-                    KNA</h2>
-                <p class="text-lg font-normal text-gray-500 light:text-gray-400 mb-4">The government has
-                    announced plans to compensate farmers who have lost crops and livestock due to the
-                    ongoing floods in the country. Agriculture Principal Secretary Dr. Paul Rono stated that
-                    most farmers in arid and semi-arid areas (ASLs) lost over 70% of their livelihoods to
-                    the rains. The Ministry is working with the private sector to secure funds for the
-                    compensation exercise and implement necessary mitigation measures. The PS is also
-                    encouraging farmers in affected areas to consider growing tuber crops that thrive during
-                    heavy rains to reduce losses.</p>
-                <a href="https://www.facebook.com/story.php?story_fbid=pfbid023uZk2Cd95bqFWbTuybERoSY56uxSKrB76AJVPRWV3S3KXpj8AYdAiNxLB4wAYyHQl&id=100069169660599&sfnsn=mo&mibextid=6aamW6&refsrc=deprecated&paipv=0&eav=Afan9ABJ0ahI9wt1_Wv1Qq3tNsveEA44l6QvJtaSnJlebbPndKeJfQor6MUob6OaYLU&_rdr"
-                    target="_blank"
-                    class="text-green-600 light:text-green-500 hover:underline font-medium text-lg inline-flex items-center">Read
-                    more
-                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
-            </div>
+                            <a href="#"
+                                class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md light:bg-gray-700 light:text-green-400 mb-2">
+                                <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 18 18">
+                                    <path
+                                        d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
+                                </svg>
+                                ASDSP
+                            </a>
+                            <h2 class="text-gray-900 light:text-white text-3xl font-extrabold mb-2">Kenya News Agency -
+                                KNA</h2>
+                            <p class="text-lg font-normal text-gray-500 light:text-gray-400 mb-4">The government has
+                                announced plans to compensate farmers who have lost crops and livestock due to the
+                                ongoing floods in the country. Agriculture Principal Secretary Dr. Paul Rono stated that
+                                most farmers in arid and semi-arid areas (ASLs) lost over 70% of their livelihoods to
+                                the rains. The Ministry is working with the private sector to secure funds for the
+                                compensation exercise and implement necessary mitigation measures. The PS is also
+                                encouraging farmers in affected areas to consider growing tuber crops that thrive during
+                                heavy rains to reduce losses.</p>
+                            <a href="https://www.facebook.com/story.php?story_fbid=pfbid023uZk2Cd95bqFWbTuybERoSY56uxSKrB76AJVPRWV3S3KXpj8AYdAiNxLB4wAYyHQl&id=100069169660599&sfnsn=mo&mibextid=6aamW6&refsrc=deprecated&paipv=0&eav=Afan9ABJ0ahI9wt1_Wv1Qq3tNsveEA44l6QvJtaSnJlebbPndKeJfQor6MUob6OaYLU&_rdr"
+                                target="_blank"
+                                class="text-green-600 light:text-green-500 hover:underline font-medium text-lg inline-flex items-center">Read
+                                more
+                                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
 
-        </div>
+                    </div>
 
 
 
--- resources/views/pages/success-inc.blade.php
+++ resources/views/pages/success-inc.blade.php
@@ -15,9 +15,7 @@
             </a>
             <h1 class="text-gray-900 light:text-white text-3xl md:text-5xl font-extrabold mb-2">Other Success
                 Stories</h1>
-            <p class="text-lg font-normal text-gray-500 light:text-gray-400 mb-6">Static websites are now
-                used to bootstrap lots of websites and are becoming the basis for a variety of tools that
-                even influence both web designers and developers.</p>
+            <p class="text-lg font-normal text-gray-500 light:text-gray-400 mb-6">The programme is implemented by the Government of Kenya (National and 47 county governments) with strong participation of the private sector as direct beneficiaries or service providers. It is financed by the Government of Kenya, Sida and EU for a period of five years (2017-2022).</p>
 
         </div>
         <div class="grid md:grid-cols-2 gap-8">
