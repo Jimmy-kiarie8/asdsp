88cd4c2f27d9d6ca3022ab860e430e7204836887
jimmy <jimmy@kali.kali>
2024-01-10 13:13:44 +0300

    commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -576,8 +576,8 @@
 
 
             {{-- Twitter --}}
-            <section class="bg-white dark:bg-gray-900" style="background: #fff;">
-                <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-12">
+            <section class="bg-white" style="background: #fff;">
+                <div class="py-8 mx-auto lg:py-16 lg:px-12">
                     <div class="text-center">
                         <a href="https://twitter.com/TIMPSPORTAL" target="_blank"
                             class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
@@ -629,10 +629,10 @@
 
 
 
-                    <div class="grid md:grid-cols-2 gap-8">
+                    <div class="grid md:grid-cols-3 gap-8">
 
                         <div
-                            class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-8 md:p-12">
+                            class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-2 ">
                             <a href="#"
                                 class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md light:bg-gray-700 light:text-green-400 mb-2">
                                 <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -646,8 +646,25 @@
                             
                         </div>
 
+
                         <div
-                            class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-8 md:p-12">
+                            class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-2 ">
+
+                            <a href="#"
+                                class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md light:bg-gray-700 light:text-green-400 mb-2">
+                                <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 18 18">
+                                    <path
+                                        d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
+                                </svg>
+                                ASDSP
+                            </a>
+                            <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Good morning? Another good reason to sample out and venture into value chains that we do promote: <br>1. Local goat meat<br>2. Indigenous chicken and,<br>3. Honey <a href="https://t.co/4kf1JTPMUq">pic.twitter.com/4kf1JTPMUq</a></p>&mdash; ASDSP II West Pokot County (@AsdspC) <a href="https://twitter.com/AsdspC/status/1353241605067714560?ref_src=twsrc%5Etfw">January 24, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                            
+                        </div>
+
+                        <div
+                            class="bg-gray-50 light:bg-gray-800 border border-gray-200 light:border-gray-700 rounded-lg p-2 ">
 
                             <a href="#"
                                 class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md light:bg-gray-700 light:text-green-400 mb-2">
@@ -661,7 +678,6 @@
                             <blockquote class="twitter-tweet"><p lang="zxx" dir="ltr"><a href="https://t.co/9a0W6a7dB3">pic.twitter.com/9a0W6a7dB3</a></p>&mdash; ASDSP II (@TIMPSPORTAL) <a href="https://twitter.com/TIMPSPORTAL/status/1744350235986428033?ref_src=twsrc%5Etfw">January 8, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                             
                         </div>
-
                     </div>
                 </div>
             </section>
