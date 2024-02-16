a2d05fd188517405f220d03464a05b7b175be63f
jimmy <jimmy@kali.kali>
2024-02-16 11:50:42 +0300

    commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -107,7 +107,6 @@
                         @foreach ($inovations as $item)
                             <div>
 
-
                                 <div
                                     class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
